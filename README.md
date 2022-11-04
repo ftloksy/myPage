7d3a811c748ec21859dda99df1435f1f61dba320
Frankie Chow <freakchow@gmail.com>
2022-11-04 17:43:56 +0000

    This is the base myPage. haven't any contents.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # myPage
 This is a webpage about me.
+and this is request for my course
+L1T24 task.
---
+++ script/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+    $("bio").load("includeHtml/bio.html", function() {
+//        $("img#biolab").attr("right", "0px");
+    });
+	// load the header. in header, there is my logo, webpage name
+	// and a dropdown menu. because header and photo is in different <div>
+	// so If I refresh the photo display, will not clear the header's event handler.
+	$("header").load("includeHtml/header.html", function() {
+        $("myName").load("includeHtml/myName.html", function() {
+            $("logo").load("svg/frankie.svg");
+        });
+/*         $("navbar").load("includeHtml/navigationBar.html", function() {
+            $("nav.dropdown").click(function() {
+				$(this).children("div").toggle("slow");
+			});  
+        }); */
+	});
+
+    $("nav").load("includeHtml/nav.html", function() {
+        $("img#bio").on("click", function() {
+            console.log($("img#bio").attr("src"));
+            console.log($("img#biolab").attr("src"));
+            $("img#biolabtwo").animate({
+                right: "1000px",
+            }).css("display", "none");
+
+            $("img#biolab").css("display", "block").animate({
+                right: "0px",
+            })
+        });
+
+        $("img#biotwo").on("click", function() {
+            $("img#biolab").animate({
+                right: "1000px",
+            }).css("display", "none");
+
+            $("img#biolabtwo").css("display", "block").animate({
+                right: "0px",
+            })
+        })
+    });
+    $("footer").load("includeHtml/footer.html");
+    
+});
